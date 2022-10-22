cd181bcebb418d32d93e941598fc4ec482ae6ae3
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-10-22 09:28:41 +0200

    Add example with scaling_mode
    It's currently broken, because we don't accound for it when panning the
    camera.

---
+++ examples/camera_scaling_mode.rs
@@ -0,0 +1,39 @@
+use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy_pancam::{PanCam, PanCamPlugin};
+use rand::prelude::random;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PanCamPlugin::default())
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    let mut cam = Camera2dBundle::default();
+    cam.projection.scaling_mode = ScalingMode::FixedVertical(10.0);
+
+    commands.spawn_bundle(cam).insert(PanCam::default());
+
+    let n = 20;
+    let spacing = 1.;
+    let offset = spacing * n as f32 / 2.;
+    let custom_size = Some(Vec2::new(1.0, 1.0));
+    for x in 0..n {
+        for y in 0..n {
+            let x = x as f32 * spacing - offset;
+            let y = y as f32 * spacing - offset;
+            let color = Color::hsl(240., random::<f32>() * 0.3, random::<f32>() * 0.3);
+            commands.spawn_bundle(SpriteBundle {
+                sprite: Sprite {
+                    color,
+                    custom_size,
+                    ..default()
+                },
+                transform: Transform::from_xyz(x, y, 0.),
+                ..default()
+            });
+        }
+    }
+}
