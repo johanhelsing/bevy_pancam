5ccabe28ff046694d0a8b37424ce6c4c7523cef1
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2024-09-03 09:00:56 +0200

    docs: Add min/max limits to viewport example

--- examples/viewport.rs
+++ examples/viewport.rs
@@ -22,27 +22,34 @@ fn setup(mut commands: Commands) {
             },
             ..default()
         },
-        PanCam::default(),
+        PanCam {
+            min_x: -500.,
+            min_y: -500.,
+            max_x: 500.,
+            max_y: 500.,
+            ..default()
+        },
     ));
 
-    let n = 20;
-    let spacing = 50.;
-    let offset = spacing * n as f32 / 2.;
-    let custom_size = Some(Vec2::new(spacing, spacing));
-    for x in 0..n {
-        for y in 0..n {
-            let x = x as f32 * spacing - offset;
-            let y = y as f32 * spacing - offset;
-            let color = Color::hsl(240., random::<f32>() * 0.3, random::<f32>() * 0.3);
-            commands.spawn(SpriteBundle {
-                sprite: Sprite {
-                    color,
-                    custom_size,
-                    ..default()
-                },
-                transform: Transform::from_xyz(x, y, 0.),
-                ..default()
-            });
-        }
-    }
+    // background
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::srgb(0.3, 0.3, 0.3),
+            custom_size: Some(Vec2::new(1000., 1000.)),
+            ..default()
+        },
+        transform: Transform::from_xyz(0., 0., 0.),
+        ..default()
+    });
+
+    // red square
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::srgb(0.8, 0.3, 0.3),
+            custom_size: Some(Vec2::new(100., 100.)),
+            ..default()
+        },
+        transform: Transform::from_xyz(0., 0., 1.),
+        ..default()
+    });
 }
